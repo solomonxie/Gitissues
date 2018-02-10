f92c37aa94a8076713e583e7664c97ed7e2cabca
Solomon Xie <solomonxiewise@gmail.com>
2018-02-10 13:55:39 +0800

    git module study

---
+++ git-repo-sync-test.py
@@ -0,0 +1,11 @@
+import pdb; pdb.set_trace()
+
+import git
+
+path = '/Volumes/SD/Workspace/tests/test-git-remove-all' 
+repo = git.Repo.init(path=path)
+
+print type(repo)
+print type(repo)
+print type(repo)
+print type(repo)
