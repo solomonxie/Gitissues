fa268473e481a7f56bd3e1044d90c856a53722f4
Solomon Xie <solomonxiewise@gmail.com>
2018-02-13 18:46:39 +0800

    independantly tested three files and passed: fetching, mapping, synchronising

--- remote-repo-syncing.py
+++ remote-repo-syncing.py
@@ -20,43 +20,24 @@ def main():
 
     # @@ load config variables 
     fetched_user = config['fetch']['user']
-    root         = config['fetch']['root_dir']
+    root         = config['local']['root_dir']
     today        = str(date.today())
 
     # @@ connect, init or clone to a local repo directory
     repo = git.Repo.init(root)
 
-    # (Workflow: before download new data, commit local repo first, then pull remote changes, then download new data and push to remote)
-
-    # @ check untracked files and commit 
+    # @@ check untracked files and commit 
     repo.git.add('.')
     repo.git.commit(m='Commit before fetching new on [%s]'%today)
 
-    # @ setup remote address and authentication
+    # @@ setup remote connection
     remote = repo.remote()
 
-    # @ pull changes from remote and solve conflicts
+    # @@ pull changes from remote and solve conflicts
     remote.pull()
     
-    # @ clear directory before download new data. Not blindly remove everything but only remove repos will be renewed
-    shutil.rmtree(root+'/'+fetched_user)
-    
-
-    # @ fetching issues from internet
-
-
-    # @ mapping json dato to markdown files
-
-
-    # @ commit newly fetched changes to local repo
-    os.system('git add .')
-    os.system('git commit -m "Commit newly fetched data on [%s]"'%today)
-    #repo.git.add('.')
-    #repo.git.commit(m='Commit newly fetched data on [%s]'%today)
-
-    # @ push to remote repo
-    os.system('git push')
-    #remote.push()
+    # @@ push to remote repo
+    remote.push()
 
 
 
