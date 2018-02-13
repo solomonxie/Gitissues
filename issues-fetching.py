2436590a66a43146b831cbb0e60d2d0588c761fb
Solomon Xie <solomonxiewise@gmail.com>
2018-02-13 17:42:52 +0800

    failed on distinguish updates and deletions

--- issues-fetching.py
+++ issues-fetching.py
@@ -16,7 +16,8 @@ def main():
 
 def fetch_issues():
     """
-    Fetch one repo's issues @2 archive to a local folder @3 pack into a zip file
+    FETCH one repo's issues, @2 archive to a local folder
+    IF there's no change from internet, then abord fetching and writing to local files
     """
 
     # @@ load local config file
@@ -31,7 +32,6 @@ def fetch_issues():
     repo_dir     = '%s/%s/%s'%(root,user,repo)
     today        = str(date.today())
 
-    import pdb; pdb.set_trace() # debugging mode
 
     # @@ retrieving data from internet, @ with response validation
     r = requests.get(issues_url,timeout=10)
@@ -39,27 +39,48 @@ def fetch_issues():
         print 'Failed on fetching [%s] due to unexpected response'%issues_url
         return False
 
-    # @ check if there's change from internet, abord mission if there's no change.
+    # @@ check if there's change from internet, abord mission if there's no change.
+    updates = []
+    deletions = []
     if os.path.exists(repo_dir+'/log/issues.json') is True:
+
+
         with open(repo_dir+'/log/issues.json', 'r') as f:
-            if json.loads(f.read()) is r.json(): return         # stop further fetching if no new thing added
+            # @@ compare two json files with updated time. (can't directly compare two json files)
+            #old = set([d['updated_at'] for d in json.loads(f.read())])
+            #new = set([d['updated_at'] for d in r.json()])
+            old = [{iss['number']:iss['updated_at']} for iss in json.loads(f.read())]
+            new = [{iss['number']:iss['updated_at']} for iss in r.json()]
+
+        for iss in r.json():
+            if 
+
+        #ups  = new - old       # udpates and adds
+        #dels = old - new       # deletions
+
+        # quit, if no changes at all
+        #if len(ups) is 0 and len(dels) is 0: return    
+
+        # filter out updates and deletions
+        #updates = [iss for iss in r.json() if iss['updated_at'] in ups]
+
     else:
         # @@ log issues as original json file, for future restoration or further use
         os.makedirs(repo_dir+'/log')
         with open(repo_dir+'/log/issues.json', 'w') as f:
             f.write(r.content)
 
-    # @ load issues and record amount for comparision
-    issues = r.json()
-    n = 0
+        updates = r.json()
 
+    import pdb; pdb.set_trace() # debugging mode
+    
     # @@ iterate each issue for further fetching
-    for issue in issues:
-        n    += 1
+    for issue in updates:
         title = issue['title']
         info  = issue['body']
         index = issue['number']
         comments_url = issue['comments_url']
+        counts = issues['comments']
 
         # @ pause for awhile before fetch to reduce risk of being banned from server
         time.sleep(1)     # sleep 60sec
@@ -68,7 +89,7 @@ def fetch_issues():
         _r = requests.get(comments_url,timeout=10)
         if _r.status_code is not 200:
             print 'Failed on fetching [%s] due to enexpected response'%comments_url
-            return False
+            return False              # if failed one comment, then restart whole process on this issue
 
         # @ log comments as original json file, for future restoration or further use
         with open(repo_dir+'/log/issue-%d.json'%index, 'w') as f:
@@ -76,10 +97,7 @@ def fetch_issues():
 
         print '%d comments for issue-%d[%s] fetched.'%(len(_r.json())+1,index, title)
 
-    if n == len(issues):
-        print 'all %d issues for %s fetched.'%(n,repo)
-    else:
-        print '[%d] issues not been fetched.'%(len(issues)-n)
+    print 'all %d issues for %s fetched.'%(counts,repo)
 
 
 
