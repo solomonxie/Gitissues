197757134fe2dadbac41c23954a221925c296cae
Solomon Xie <solomonxiewise@gmail.com>
2018-05-24 18:04:51 +0800

    moved issues.json to the logging folder, out of the git repo area.

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 # ==== This project is required to work in Python2 enviroment, preferred Virtualenv enviroment. ====
+"""
+How this module works:
+"""
 
 import os
 import sys
@@ -14,17 +17,20 @@ class Config:
     def __init__(self):
         with open('/Volumes/SD/Workspace/etc/gitissues-config.json', 'r') as f:
             cfg = json.loads(f.read())
+        # github api related configs
         self.user = cfg["fetch"]["user"].encode('utf-8')
         self.repo = cfg["fetch"]["repo"].encode('utf-8')
         self.issues_url = cfg["fetch"]["issues_url"].encode('utf-8')
         self.auth = cfg["fetch"]["auth"].encode('utf-8')
+        # remote storage configs (a different git repo with the original one)
         self.remote_url = cfg["remote"]["ssh"].encode('utf-8')
         self.remote_user = cfg["remote"]["user"].encode('utf-8')
         self.email = cfg["remote"]["email"].encode('utf-8')
+        # download resources to local drive
         self.root = cfg["local"]["root_dir"].encode('utf-8')
-        self.repo_dir     = "%s/%s/%s" % (self.root, self.user, self.repo)
-        self.issues_path  = self.repo_dir + '/issues.json'
+        self.repo_dir     = cfg["local"]["repo_dir"].encode('utf-8')
         self.log_dir = cfg["local"]["log_dir"].encode('utf-8')
+        self.issues_path  = cfg["local"]["issues_path"].encode('utf-8')
 
         # init universial logger for modules
         define_logger('gitissues', self.log_dir)
