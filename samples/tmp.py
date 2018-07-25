2159e557398d8c93fd588867906dee2a3f7f4e28
Solomon Xie <solomonxiewise@gmail.com>
2018-07-25 16:39:49 +0800

    Update from Solomon-mba.local

--- samples/tmp.py
+++ samples/tmp.py
@@ -1,29 +1,43 @@
 import re
 import yaml
 
-_front_matter = None
-text_issue = ''
-with open('samples/sample-issue.md') as f:
-    text_issue = f.read()
+def __get_jekyll_front_matter():
+    _front_matter = None
+    text_issue = ''
+    with open('samples/sample-issue.md') as f:
+        text_issue = f.read()
 
-regex = r'^\s*<!--.*\n^---$([\w\W]*)^---$\n-->\s*$'
-matches = re.findall(regex, text_issue, re.MULTILINE) 
+    regex = r'^\s*<!--.*\n^---$([\w\W]*)^---$\n-->\s*$'
+    matches = re.findall(regex, text_issue, re.MULTILINE) 
+    for m in matches:
+        _front_matter = m.strip()
 
-for m in matches:
-    _front_matter = m.strip()
+    obj_front_matter = yaml.load(_front_matter)
+    print(obj_front_matter)
+    # {'layout': 'post', 'title': None, 'image': None, 'description': None, 'categories': ['Calculus', 'Math', 'Khan Academy']} categories: [Calculus, Math, Khan Academy]
 
-print(_front_matter)
+    _yml_front_matter = yaml.dump(obj_front_matter)
+    print(_yml_front_matter)
+
+# Run
+__get_jekyll_front_matter()
 
 
-_title = None
-text_title = ''
-with open('samples/sample-issue.md') as f:
-    text_title = f.read()
 
-regex = r'^\#\s+(.+)$' 
-matches = re.findall(regex, text_title, re.MULTILINE) 
 
-for m in matches:
-    _title = m.strip()
 
-print(_title)
+def _get_title():
+    _title = None
+    text_title = ''
+    with open('samples/sample-issue.md') as f:
+        text_title = f.read()
+
+    regex = r'^\#\s+(.+)$' 
+    matches = re.findall(regex, text_title, re.MULTILINE) 
+
+    for m in matches:
+        _title = m.strip()
+
+    print(_title)
+
+# _get_title()
