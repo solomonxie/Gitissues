8575307f03f3e417cefd17cb0ecf988d9813080a
Solomon Xie <solomonxiewise@gmail.com>
2020-03-09 21:41:33 +0800

    add git methods

--- all_in_one.py
+++ all_in_one.py
@@ -3,15 +3,47 @@ import requests
 from retry import retry
 from logging import Logger
 from slugify import slugify
+import shutil
 
 logger = Logger(__name__)
 
 USER = os.environ.get('GITHUB_USER') or 'solomonxie'
 REPO = os.environ.get('GITHUB_REPO') or 'solomonxie.github.io'
-HEADERS = {'Authorization': 'token ' + os.environ['ACCESS_TOKEN']}
+BAK_REPO = os.environ.get('GITHUB_REPO') or 'git@github.com:solomonxie/issues_blog.git'
+HEADERS = {'Authorization': 'token ' + os.environ['TOKEN']}
 
 ISSUES_URL = f'https://api.github.com/repos/{USER}/{REPO}/issues'
-ROOT = os.environ.get('BAK_ROOT') or './dataset'
+ROOT = os.environ.get('BAK_ROOT') or '/tmp'
+BLOG = os.path.join(ROOT, 'blog')
+CACHE = '/tmp/blog'
+
+
+@retry((Exception, ), tries=3, delay=3, jitter=5)
+def init_bak_repo():
+    """
+    Initialize backup-repo
+    """
+    if os.path.exists(os.path.join(ROOT, '.git')):
+        return
+    # raise Exception('Not a git repo for backup: {}'.format(os.path.abspath(ROOT)))
+    p = os.popen('git clone --depth 1 {BAK_REPO} {ROOT} 2>&1')
+    logger.critical(p.read())
+
+
+@retry((Exception, ), tries=3, delay=3, jitter=5)
+def sync_bak_repo():
+    os.system('git -C {ROOT} reset --hard master')
+    os.system('git -C {ROOT} clean -fd')
+    p = os.popen('git -C {ROOT} pull origin master 2>&1')
+    logger.critical(p.read())
+
+
+@retry((Exception, ), tries=3, delay=3, jitter=5)
+def publish_bak_repo():
+    p = os.popen('git -C {ROOT} add . 2>&1')
+    p = os.popen('git -C {ROOT} commit -m "" 2>&1')
+    p = os.popen('git -C {ROOT} push --force origin master 2>&1')
+    logger.critical(p.read())
 
 
 @retry((Exception, ), tries=3, delay=3, jitter=5)
@@ -36,7 +68,7 @@ def download_comments(issue):
 
 def save_issue_body(issue):
     issue['name'] = '{}-{}'.format(issue['number'], slugify(issue['title']))
-    path = os.path.join(ROOT, issue['name'], 'README.md')
+    path = os.path.join(CACHE, issue['name'], 'README.md')
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
 
@@ -48,21 +80,37 @@ def save_issue_body(issue):
 
 def save_comment(issue, comment):
     comment['name'] = '{}-{}'.format(comment['id'], slugify(comment['body'][:30]))
-    path = os.path.join(ROOT, issue['name'], comment['name'] + '.md')
+    path = os.path.join(CACHE, issue['name'], comment['name'] + '.md')
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
 
     with open(path, 'w') as f:
         f.write(comment['body'] + '\n')
-    logger.critical('\t Saved a comment to: ' + path)
+    logger.info('\t Saved a comment to: ' + path)
     return path
 
 
 def main():
     logger.critical('Start running program...')
+    init_bak_repo()
+
     issue_list = download_issues()
     for issue in issue_list:
         download_comments(issue)
+    logger.critical('Finished downloading.')
+
+    # Remove existing blog folder (much easier than diff)
+    if os.path.exists(BLOG):
+        shutil.rmtree(BLOG)
+        logger.critical('Removed existing folder: ', + BLOG)
+
+    # Move newly retrieved files from cache to backup folder
+    shutil.move(CACHE, BLOG)
+    logger.critical('Replaced existing files with newly retrieved files')
+
+    # Update Git backup-repo
+    os.system('')
+
     logger.critical('Finished whole program.')
 
 
