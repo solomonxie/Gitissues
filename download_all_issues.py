fe1f32d14865a847dead49c1741929d4b5c39e4e
Solomon Xie <solomonxiewise@gmail.com>
2023-10-13 10:48:07 -0700

    complete

--- download_all_issues.py
+++ download_all_issues.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from slugify import slugify
 
 from utils.common_utils import read_envfile
 
@@ -12,20 +13,40 @@ HEADERS = {'Authorization': 'token ' + os.environ['GH_TOKEN']}
 
 def main():
     resp = requests.get(ISSUES_URL, headers=HEADERS)
-    issue_list = resp.json()
-    for issue in issue_list:
-        issue_title = issue['title']
+    for issue in resp.json():
+        title = issue['title']
+        print(f'Loading issue: [{title}]')
         created_at = issue['created_at']
-        lables = sorted([lb['name'] for lb in issue['labels']])
+        tags = ' '.join(['#' + lb['name'].replace(' ', '') for lb in issue['labels']])
+        body = issue.get('body') or ''
+        content = f'> tags: {tags}\n> created at: {created_at}\n{body}'
+        issue_dir = f'/tmp/issues_blog/{slugify(title)}'
+        os.makedirs(issue_dir, exist_ok=True)
+        open(f'{issue_dir}/README.md', 'w').write(content)
         cm_url = issue['comments_url']
         cm_count = issue['comments']
         cm_limit = 30
         cm_pages = int((cm_count + abs(cm_count % cm_limit - cm_limit)) / cm_limit)
         comment_list = []
-        for p in range(0, cm_pages):
-            cm_resp = requests.get(f'{cm_url}?page={p+1}&per_page={cm_limit}', headers=HEADERS)
-            comment_list += [{'body': cm['body'], 'created_at': cm['created_at']} for cm in cm_resp.json()]
+        for p in range(1, cm_pages+1):
+            cm_resp = requests.get(f'{cm_url}?page={p}&per_page={cm_limit}', headers=HEADERS)
+            for cm in cm_resp.json():
+                cm_body = cm['body']
+                stop = min(max(cm_body.find('\r'), 0), max(cm_body.find('\n'), 0))
+                cm_idx = str(len(comment_list) + 1)
+                cm_title = cm_idx + '-' + slugify(cm_body[:stop]).strip()
+                cm_content = '{}\n\n{}'.format(cm['created_at'], cm_body)
+                comment_list.append({
+                    'body': cm_body,
+                    'title': cm_title,
+                    'created_at': cm['created_at'],
+                })
+                cm_path = f'{issue_dir}/{cm_title[:128]}.md'
+                open(cm_path, 'w').write(cm_content)
+                print(f'Saved comment to: {cm_path}')
+        print('='*80)
 
 
 if __name__ == '__main__':
     main()
+    print('ok.')
